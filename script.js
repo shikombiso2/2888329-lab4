645ee99328750d0812c3487d486ec9d0ad55e06d
shikombiso2 <2888329@students.wits.ac.za>
2026-03-02 16:34:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function searchCountry(country) {
 
     try {
         
-        const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
         
         if (!response.ok) {
             throw new Error("Country not found");
